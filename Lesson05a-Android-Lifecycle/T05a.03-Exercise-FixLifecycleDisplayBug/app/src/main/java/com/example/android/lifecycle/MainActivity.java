68fbc965557cad4c0d74a1de4f33e133f6c2bd7d
George Vrynios <gvrynios@gmail.com>
2017-11-27 23:05:24 +0200

    Completing Lesson 6 TODOs

--- Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
+++ Lesson05a-Android-Lifecycle/T05a.03-Exercise-FixLifecycleDisplayBug/app/src/main/java/com/example/android/lifecycle/MainActivity.java
@@ -6,6 +6,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
 
     /*
@@ -39,7 +41,8 @@ public class MainActivity extends AppCompatActivity {
      */
     private TextView mLifecycleDisplay;
 
-    // TODO (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    // COMPLETED (1) Declare and instantiate a static ArrayList of Strings called mLifecycleCallbacks
+    private static ArrayList<String> mLifecycleCallbacks = new ArrayList<String>();
 
     /**
      * Called when the activity is first created. This is where you should do all of your normal
@@ -71,9 +74,20 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
-        // TODO (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
+        // COMPLETED (4) Iterate backwards through mLifecycleCallbacks, appending each String and a newline to mLifecycleDisplay
 
-        // TODO (5) Clear mLifecycleCallbacks after iterating through it
+        for (int i = mLifecycleCallbacks.size() - 1; i >= 0; i--) {
+            mLifecycleDisplay.append(mLifecycleCallbacks.get(i) + "\n");
+        }
+
+        /*
+        for (String lifecycleCallback : mLifecycleCallbacks) {
+            mLifecycleDisplay.append(lifecycleCallback + "\n");
+        }
+        */
+
+        // COMPLETED (5) Clear mLifecycleCallbacks after iterating through it
+        mLifecycleCallbacks.clear();
 
         logAndAppend(ON_CREATE);
     }
@@ -133,7 +147,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onStop() {
         super.onStop();
 
-        // TODO (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        // COMPLETED (2) Add the ON_STOP String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(0, ON_STOP);
+        //mLifecycleCallbacks.add(ON_STOP);
 
         logAndAppend(ON_STOP);
     }
@@ -146,6 +162,8 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onRestart() {
         super.onRestart();
+
+        mLifecycleCallbacks.add(0, ON_RESTART);
 
         logAndAppend(ON_RESTART);
     }
@@ -160,7 +178,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onDestroy() {
         super.onDestroy();
 
-        // TODO (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        // COMPLETED (3) Add the ON_DESTROY String to the front of mLifecycleCallbacks
+        mLifecycleCallbacks.add(0, ON_DESTROY);
+        //mLifecycleCallbacks.add(ON_DESTROY);
 
         logAndAppend(ON_DESTROY);
     }
--- Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
+++ Lesson05b-Smarter-GitHub-Repo-Search/T05b.01-Exercise-SaveResults/app/src/main/java/com/example/android/asynctaskloader/MainActivity.java
@@ -32,9 +32,11 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
-    // TODO (1) Create a static final key to store the query's URL
+    // COMPLETED (1) Create a static final key to store the query's URL
+    static final String SEARCH_QUERY_KEY = "query";
 
-    // TODO (2) Create a static final key to store the search's raw JSON
+    // COMPLETED (2) Create a static final key to store the search's raw JSON
+    static final String SEARCH_RESULTS_KEY = "results";
 
     private EditText mSearchBoxEditText;
 
@@ -59,7 +61,11 @@ public class MainActivity extends AppCompatActivity {
 
         mLoadingIndicator = (ProgressBar) findViewById(R.id.pb_loading_indicator);
 
-        // TODO (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        // COMPLETED (9) If the savedInstanceState bundle is not null, set the text of the URL and search results TextView respectively
+        if (savedInstanceState != null) {
+            mUrlDisplayTextView.setText(savedInstanceState.getString(SEARCH_QUERY_KEY));
+            mSearchResultsTextView.setText(savedInstanceState.getString(SEARCH_RESULTS_KEY));
+        }
     }
 
     /**
@@ -151,13 +157,24 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    // TODO (3) Override onSaveInstanceState to persist data across Activity recreation
-    // Do the following steps within onSaveInstanceState
-    // TODO (4) Make sure super.onSaveInstanceState is called before doing anything else
 
-    // TODO (5) Put the contents of the TextView that contains our URL into a variable
-    // TODO (6) Using the key for the query URL, put the string in the outState Bundle
+    // COMPLETED (3) Override onSaveInstanceState to persist data across Activity recreation
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        // Do the following steps within onSaveInstanceState
+        // COMPLETED (4) Make sure super.onSaveInstanceState is called before doing anything else
+        super.onSaveInstanceState(outState);
 
-    // TODO (7) Put the contents of the TextView that contains our raw JSON search results into a variable
-    // TODO (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        // COMPLETED (5) Put the contents of the TextView that contains our URL into a variable
+        String urlString = mUrlDisplayTextView.getText().toString();
+
+        // COMPLETED (6) Using the key for the query URL, put the string in the outState Bundle
+        outState.putString(SEARCH_QUERY_KEY, urlString);
+
+        // COMPLETED (7) Put the contents of the TextView that contains our raw JSON search results into a variable
+        String resultsString = mSearchResultsTextView.getText().toString();
+
+        // COMPLETED (8) Using the key for the raw JSON search results, put the search results into the outState Bundle
+        outState.putString(SEARCH_RESULTS_KEY, resultsString);
+    }
 }
